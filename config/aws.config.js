126579dc763c267af04dcf2e320e85ff773ccf80
cassac <hicassac@gmail.com>
2016-09-14 15:25:09 -0700

    update

--- config/aws.config.js
+++ config/aws.config.js
@@ -4,11 +4,14 @@ var AWS = require('aws-sdk');
 var AWS_ACCESS_KEY_ID = '';
 var AWS_SECRET_ACCESS_KEY = ''; 
 var AWS_REGION = '';
+var BASE_URL = '';
 
 AWS.config.update({
 	accessKeyId: AWS_ACCESS_KEY_ID, 
 	secretAccessKey: AWS_SECRET_ACCESS_KEY,
 	region: AWS_REGION
 });
+
+AWS.config.baseUrl = BASE_URL;
 
 module.exports = AWS;
