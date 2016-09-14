3e050739d2cc91d9a9734552c551914dd8aabeca
Kevin Chan <kevindchan@gmail.com>
2016-09-14 10:51:35 -0700

    moved db setup to server.js

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ var port = process.env.PORT || 3000;
 require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
 
+var pg = require('pg');
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABSE_URL, function(err, client) {
+  if (err) {
+    console.log('error connection pg: ', err)
+  }
+  console.log('connected pg! changes');
+  // client
+  //test
+})
+
 app.listen(port, function(){
   console.log("Listening on port " + port);
 })
